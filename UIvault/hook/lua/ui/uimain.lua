636e123b98baa8ce842ab1e1da2da250f9692aeb
Illumination <74865202+BornOfSet@users.noreply.github.com>
2022-09-14 01:00:04 +0800

    Update uimain.lua

--- UIvault/hook/lua/ui/uimain.lua
+++ UIvault/hook/lua/ui/uimain.lua
@@ -4,3 +4,4 @@ StartGameUI = function()
 	doscript('/mods/UIvault/local/localization.lua')
 end
 --I'd like to see a clean and tidy code layout . I hate hooking too much into the og game files , because I have OCD XD
+--Don't change this hook to uimain.lua . Uimain.lua is made by game and lobby , however , it's only in the game that GetFrame() works
