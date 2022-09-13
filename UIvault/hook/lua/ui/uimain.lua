a367b953e99082e34f6886d2e173bf977055e551
Illumination <74865202+BornOfSet@users.noreply.github.com>
2022-09-14 00:58:42 +0800

    Update uimain.lua

--- UIvault/hook/lua/ui/uimain.lua
+++ UIvault/hook/lua/ui/uimain.lua
@@ -3,3 +3,4 @@ StartGameUI = function()
 	background()
 	doscript('/mods/UIvault/local/localization.lua')
 end
+--I'd like to see a clean and tidy code layout . I hate hooking too much into the og game files , because I have OCD XD
